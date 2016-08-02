dd7dbad43f148eb03bda11243cc99773f3114a5f
Andrei Pfeiffer <andrei@upsidedown.ro>
2016-08-02 11:07:32 +0300

    Rename files

--- app/src/app.js
+++ app/src/app.js
@@ -1,8 +1,51 @@
-var words = [
-    { word: 'caine', description: 'un animal care face ham ham' },
-    { word: 'masa', description: 'se pun farfuriile pe ea' }
-];
+var lettersFound = [];
+var currentWord, $wrapper;
 
-import { init } from 'src/app-es6-module.js';
+function init(wrapper, list) {
+    currentWord = list[getRandom(list.length)];
+    $wrapper    = wrapper; 
 
-init( $('#app'), words );
+    $wrapper.append('<ul>');
+
+    currentWord.word.split('').forEach((letter) => {
+        $wrapper.find('ul').append('<li>_</li>');
+    });
+
+    // $wrapper.find('ul').append( new Array(currentWord.word.length).fill('<li>_</li>').join('') );
+
+    $('<q>')
+        .text( currentWord.description )
+        .appendTo( $wrapper );
+
+    $('body').on('keyup', (e) => {
+        var key = String.fromCharCode(e.which).toLowerCase();
+        checkKey( key );
+    });
+}
+
+function checkKey(key) {
+    var index = currentWord.word.indexOf(key);
+
+    if ( currentWord.word.includes(key) ) {
+        if ( lettersFound.includes(key) ) {
+            console.log('duplicate');
+        } else {
+            lettersFound.push(key);
+            console.log(`exists on position ${index}`);
+
+            $wrapper.find('li:eq('+index+')').text(key);
+
+            if ( lettersFound.length === currentWord.word.length  ) {
+                alert('GUESSED');
+            }
+        }
+    } else {
+        console.log(`letter ${key} does not exist`);
+    }
+}
+
+function getRandom(max) {
+    return Math.floor(Math.random() * max);
+}
+
+export { init };
---
+++ app/src/main.js
@@ -0,0 +1,8 @@
+var words = [
+    { word: 'caine', description: 'un animal care face ham ham' },
+    { word: 'masa', description: 'se pun farfuriile pe ea' }
+];
+
+import { init } from 'src/app.js';
+
+init( $('#app'), words );
